efb17ac34c003a09e9b8e5a6d0d66465c7f202ce
Mukesh Kumar Shah <130866090+mukeshshah08@users.noreply.github.com>
2026-03-01 19:10:44 +0530

    Refactor imports and update image display settings

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
 import streamlit as st
+import numpy as np
+import tensorflow as tf
+from PIL import Image
 import gdown
 import os
 
@@ -12,7 +14,6 @@ def load_model():
         url = f"https://drive.google.com/uc?id={FILE_ID}"
         gdown.download(url, MODEL_PATH, quiet=False)
     return tf.keras.models.load_model(MODEL_PATH)
-
 
 model = load_model()
 
@@ -28,7 +29,7 @@ uploaded_file = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     img = image.resize((224, 224))
     img_array = np.array(img)
